e0282fc7c70a41510d3fd4c6e5ef36431986eefe
sta <sta.blockhead@gmail.com>
2025-11-11 21:46:42 +0900

    [Modify] Replace it

--- websocket-sharp/Server/WebSocketBehavior.cs
+++ websocket-sharp/Server/WebSocketBehavior.cs
@@ -1573,7 +1573,7 @@ namespace WebSocketSharp.Server
     /// </exception>
     protected void SendAsync (Stream stream, int length, Action<bool> completed)
     {
-      if (_websocket == null) {
+      if (!_registered) {
         var msg = "The session has not started yet.";
 
         throw new InvalidOperationException (msg);
