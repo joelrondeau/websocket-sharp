d4545cd99088342b08df5469815377084fe2149e
sta <sta.blockhead@gmail.com>
2025-12-08 22:17:20 +0900

    [Modify] Edit it

--- websocket-sharp/WebSocket.cs
+++ websocket-sharp/WebSocket.cs
@@ -3730,8 +3730,8 @@ namespace WebSocketSharp
     /// <paramref name="fileInfo"/> is <see langword="null"/>.
     /// </exception>
     /// <exception cref="InvalidOperationException">
-    /// The Send method is not available when the current state of
-    /// the interface is not Open.
+    /// This method is not available when the current state of the interface
+    /// is not Open.
     /// </exception>
     public void Send (FileInfo fileInfo)
     {
